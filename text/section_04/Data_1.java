18f1a5e8b2ce86d6f60397d40a1ab1a859d52ee6
J-akayama <ta.200206@gmail.com>
2023-10-17 20:58:39 +0900

    commit

---
+++ text/section_04/Data_1.java
@@ -0,0 +1,26 @@
+package text.section_04;
+
+public class Data_1 {
+
+	public static void main(String[] args) {
+		System.out.println(100);
+		System.out.println(0);
+		System.out.println(-123);
+		System.out.println(2147483648L);
+		System.out.println(0.1);
+        System.out.println(1.618);
+        System.out.println(-9876.0123);
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println("こんにちは");
+        System.out.println(1+100);
+        System.out.println(45+18);
+        System.out.println(30-12);
+        System.out.println(15*6);
+        System.out.println(30/3);
+        System.out.println(45%0.8);
+        System.out.println("侍"+"テラコヤ");
+
+	}
+
+}
